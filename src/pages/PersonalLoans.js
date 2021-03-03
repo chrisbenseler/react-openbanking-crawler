dde5a4d4453513485357bc15129a0162420f790e
Chris Benseler <cbenseler@gmail.com>
2021-03-03 11:53:47 -0300

    feat: cards

--- src/pages/PersonalLoans.js
+++ src/pages/PersonalLoans.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import ServicesFees from "../components/ServicesFees";
 
 import { listPersonalLoansFromInstitution } from "../services/api";
@@ -31,28 +34,20 @@ function PersonalLoans() {
   return (
     <section>
       <h2>Empréstimo - Pessoa Física</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Tipo</th>
-            <th>Taxas</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {personalLoans.map((p, index) => (
-            <tr key={p._id + "_" + index}>
-              <td>{p.type}</td>
-              <td>
-                <ServicesFees services={p.fees.services} />
-              </td>
-              <td>
-                
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      <Grid container spacing={3}>
+      {personalLoans.map((p, index) => (
+        <Grid item xs={6}>
+        <Card key={p._id + "_" + index}>
+          <CardContent>
+            <Typography variant="span" component="h2">{p.type}</Typography>
+            <ServicesFees services={p.fees.services} />
+          </CardContent>
+        </Card>
+        </Grid>
+      ))}
+      </Grid>
+      
       <footer>
         Página {pager.current}/{pager.total}
       </footer>
